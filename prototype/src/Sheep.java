e0eba66bf82122d5fda080756a3d819d617db985
ssf-czh <819435330@qq.com>
2019-10-21 19:14:05 +0800

    原型模式（浅克隆）

---
+++ prototype/src/Sheep.java
@@ -0,0 +1,70 @@
+public class Sheep implements Cloneable{
+    private String name;
+    private Integer age;
+    private String color;
+    private Sheep friend;
+
+    public Sheep getFriend() {
+        return friend;
+    }
+
+    @Override
+    public String toString() {
+        return "Sheep{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                ", friend=" + friend +
+                '}';
+    }
+
+    public void setFriend(Sheep friend) {
+        this.friend = friend;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sheep(String name, Integer age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age.hashCode();
+        result = 31 * result + color.hashCode();
+        return result;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    //克隆该实例，使用默认的clone方法来完成
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Sheep sheep = null;
+        sheep = (Sheep) super.clone();
+        return sheep;
+    }
+}
